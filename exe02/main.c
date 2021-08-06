5e33972c4df20622b14b70da2ab92191dd88f2a4
gabrielalv <88516987+Gabriela-Valente@users.noreply.github.com>
2021-08-06 12:45:53 +0000

    exercicio 2

--- exe02/main.c
+++ exe02/main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
 int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
+   int n1, n2, n3, D;
+   scanf("%i %i %i", &n1, &n2, &n3);
+   D = n1*n2;
+   printf("R = %i\n", n3);
+   printf("V = %i\n", n2);
+   printf("T = %i\n", n1);
+   printf("D = %i\n", D);
+   printf("L = %i\n", D/n3);
    return 0;
 }
