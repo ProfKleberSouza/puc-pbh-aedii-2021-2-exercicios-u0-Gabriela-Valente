7ad65e5cdeda4145a9d33b49ad3edc49fd155aa4
gabrielalv <88516987+Gabriela-Valente@users.noreply.github.com>
2021-08-06 15:36:01 +0000

    exercicio 6

--- exe06/main.c
+++ exe06/main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 
 int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
+   int a, b, c;
+   scanf("%i %i %i", &a, &b, &c);
+   if(a < b + c && b < a + c && c < a + b){
+     if(a == b && a == c){
+       printf("TRIANGULO EQUILATERO");
+     }else if((a == b && a != c) || (a == c && a != b) || (b == c && b != a)){
+       printf("TRIANGULO ISOCELES");
+     }
+     else{
+       printf("TRIANGULO ESCALENO");
+     }
+   }
+   else{
+     printf("OS LADOS NAO FORMAM UM TRIANGULO");
+   }
    return 0;
 }
